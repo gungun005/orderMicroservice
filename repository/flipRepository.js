553c6d9de3a9c6d11507838dc9f6d8ce28b70181
gungun005 <g053780@gmail.com>
2024-06-07 22:32:58 +0530

    updated

--- repository/flipRepository.js
+++ repository/flipRepository.js
@@ -30,6 +30,29 @@ finally{
 } 
 
 };
+const getFlipOrderBySearch=async(body)=>{
+    console.log("in repository");
+   console.log("Getting Flip Order");
+   searchOptions={};
+   if(body.product != null && body.product !== "" ){
+    searchOptions.product = body.product;
+}
+   try{
+    await client.connect()
+    const db = client.db("orderMicro");
+    const coll = db.collection("orderMicro");
+    const data = await coll.find(searchOptions)
+    console.log(data)
+    return data
+}
+catch(err){
+    console.log("Error occurred")
+    return err;
+}
+finally{
+    await client.close()
+} 
+};
 const postFlipOrder=async(body)=>{
     console.log("in repository");
     console.log(body);
@@ -40,6 +63,8 @@ const postFlipOrder=async(body)=>{
         product: body.product,
         units: body.units,
         price:body.price,
+        id:body.id,
+        email:body.email,
     };
     try{
         await client.connect()
@@ -51,6 +76,7 @@ const postFlipOrder=async(body)=>{
         return data.acknowledged == true
     }
     catch(err){
+        console.log(err);
         console.log("Error occurred")
         return err;
     }
@@ -59,25 +85,27 @@ const postFlipOrder=async(body)=>{
     } 
 };
 const getFlipOrderById=async(body)=>{
-    console.log("in repository");
-    console.log(body);
-    console.log("Getting Flip Order by Id");
+    // console.log("in repository");
+    // console.log(body);
+    // console.log("Getting Flip Order by Id");
+    let searchOptions = {};
+    if(body.id != null && body.id !== ''){
+        searchOptions.id = body.id;
+    }
+    console.log(body, searchOptions,"repo");
     try{
         await client.connect()
         const db = client.db("orderMicro");
         const coll = db.collection("orderMicro");
-        const data = await coll.find().toArray()
-        console.log(data)
-        return data
-    }
-    catch(err){
-        console.log("Error occurred")
-        return err;
+        const data = await coll.find(searchOptions).toArray();
+        console.log(data);
+    }catch(err){
+        console.log(err)
+        console.log("Error in promise")
     }
     finally{
         await client.close()
-    } 
-
+    }
     };
     const postFlipOrderById=async(body)=>{
     console.log("in repository");
@@ -110,9 +138,41 @@ const getFlipOrderById=async(body)=>{
     };
         
     const getAllFlipOrder=async (body)=>{
-    console.log("in repository");
-    console.log(body);
-    return(order_list);
+    // console.log("in repository");
+    // console.log(body);
+    // console.log(order_list);
+    let searchOptions = {};
+    console.log(body, searchOptions);
+    if(body.name != null && body.name !== ''){
+        searchOptions.name = new RegExp(body.name, 'i');
+    }
+    if(body.product!= null && body.product !== ''){
+        searchOptions.product = new RegExp(body.product, 'i');
+    }
+    if(body.price != null && body.price !== ''){
+        searchOptions.price = new RegExp(body.price, 'i');
+    }
+    if(body.units!= null && body.units !== ''){
+        searchOptions.units = new RegExp(body.units, 'i');
+    }
+    if(body.id != null && body.id !== ''){
+        searchOptions.id = new RegExp(body.id, 'i');
+    }
+    if(body.email!= null && body.email !== ''){
+        searchOptions.email = new RegExp(body.email, 'i');
+    }
+    const employeeCollection = client.db("orderMicro").collection("orderMicro");
+    try{
+        const employee = await employeeCollection.find(searchOptions).toArray();
+        console.log(employee);
+       
+    }catch{
+        console.log(err);
+        console.log("Error in promise")
+    }
+    finally{
+        await client.close()
+    }
     };
         
     const cancelFlipOrder=async(body)=>{
@@ -141,6 +201,7 @@ const getFlipOrderById=async(body)=>{
     };
     module.exports={
                 getFlipOrder,
+                getFlipOrderBySearch,
                 postFlipOrder,
                 getFlipOrderById,
                 postFlipOrderById,
